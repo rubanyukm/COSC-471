1c9d16281d5397757c9edc861f473c234c5330c5
tfurey96 <98403865+tfurey96@users.noreply.github.com>
2023-03-30 17:17:54 -0400

    progress on ddl

--- bookstoreDDL.sql
+++ bookstoreDDL.sql
@@ -8,5 +8,37 @@ create table books(
     title       text,
     Author      text,
     publisher   text,
+    
+    primary key (ISBN)
+)
+create table reviews(
+    reviewID    int,
+    reviewText  text,
+    ISBN        numeric(13,0),
+
+    primary key(reviewID),
+    foreign key( ISBN) references books(ISBN)
+
+)
+
+create table orders(
+    orderID     int,
+    customerUsername    text,
+    ISBN    numeric(13,0),
+    quantity    int,
+
+    primary key(orderID,ISBN),
+    foreign key(ISBN) references books(ISBN),
+    foreign key(customerUsername) references customer(username)
+
+)
+
+create table customer(
+    username    text,
+    pin         text,
+    fname       text,
+    lname       text,
+    custaddress text,
+    city        text,        
 
 )
