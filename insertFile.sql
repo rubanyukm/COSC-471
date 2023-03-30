2a41f0cf4e3131b0967c6a1b58f423752820970e
tfurey96 <98403865+tfurey96@users.noreply.github.com>
2023-03-30 18:22:15 -0400

    added insert file

---
+++ insertFile.sql
@@ -0,0 +1,15 @@
+insert into books values(123456789123,"book1","author1","publiser1");
+insert into books values(123453389123,"book3","author2","publiser2");
+insert into books values(123411789123,"book2","author1","publiser1");
+insert into books values(122256789123,"book4","author3","publiser3");
+
+
+insert into customer values("user1","pin1","gary","smith",
+                            "1234 street1 st","ypsilanti","MI", "visa", 4444555566667777);
+insert into customer values("user2","pin2","Greg","Ory",
+                            "1234 street2 st","ypsilanti","MI", "visa", 4444555566667777);
+
+insert into reviews values(1,"book is good",123456789123);
+insert into reviews values(2,"book is meh",123456789123);
+
+insert into orders values(12, "user1", 123453389123, 3)
